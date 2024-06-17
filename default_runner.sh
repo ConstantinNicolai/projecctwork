5dae034ab6013845d89ae7d30d0b055ba25745d0
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-06-17 16:40:29 +0200

    changed back to 1 gpu

--- default_runner.sh
+++ default_runner.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=resnet101_%j.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 
 # Check if Nvidia SMI is installed
 python3 resnet101.py
