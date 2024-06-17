b3399fb1138396d86e38bb1c49dd6491d26d2511
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-06-17 15:53:58 +0200

    changed the runner to 2 gpus

--- default_runner.sh
+++ default_runner.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=brook
 #SBATCH --job-name=read_gpu_stats
-#SBATCH --output=distilbert_%j.out
+#SBATCH --outputresnet101_%j.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 
 # Check if Nvidia SMI is installed
-python3 distilbert.py
+python3 resnet101.py
