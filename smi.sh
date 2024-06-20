246f804216429236cb19a2a8a0afa5b32dcafa04
Constantin Nicolai <constantin.nicolai@stud.uni-heidelberg.de>
2024-06-20 14:14:51 +0200

    trying to achieve logging on all gpus that run training

--- smi.sh
+++ smi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --partition=brook
 #SBATCH --job-name=read_gpu_stats
-#SBATCH --output=gpu_stats_%j.out
+#SBATCH --output=rolling_output_nojobnumber.out
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
@@ -18,9 +18,37 @@ read_gpu_model() {
     echo "GPU Model: $gpu_model"
 }
 
+
+# Function to log GPU usage for each GPU on the node
+log_gpu_usage() {
+  local gpu_ids=(${CUDA_VISIBLE_DEVICES//,/ })
+  for gpu_id in "${gpu_ids[@]}"; do
+    nvidia-smi -i ${gpu_id} -lms=1 --query-gpu=timestamp,utilization.gpu,power.draw,memory.used,memory.total --format=csv,noheader,nounits >> logs/gpu_usage_node${SLURM_NODEID}_gpu${gpu_id}.log &
+  done
+}
+
 # Main script
 read_gpu_model
+log_gpu_usage
+
+python3 resnet_multi.py
 
 # Run nvidia-smi continuously
-nvidia-smi -lms=1 --query-gpu=timestamp,utilization.gpu,power.draw,memory.used,memory.total --format=csv,noheader,nounits
+# nvidia-smi -lms=1 --query-gpu=timestamp,utilization.gpu,power.draw,memory.used,memory.total --format=csv,noheader,nounits
 
+# local gpu_ids=(${CUDA_VISIBLE_DEVICES//,/ })
+# for gpu_id in "${gpu_ids[@]}"; do
+#     nvidia-smi -i ${gpu_id} -lms=1 --query-gpu=timestamp,utilization.gpu,power.draw,memory.used,memory.total --format=csv,noheader,nounits >> logs/gpu_usage_node${SLURM_NODEID}_gpu${gpu_id}.log
+# done
+
+
+# # Function to log GPU usage for each GPU on the node
+# log_gpu_usage() {
+#   local gpu_ids=(${CUDA_VISIBLE_DEVICES//,/ })
+#   for gpu_id in "${gpu_ids[@]}"; do
+#     while true; do
+#       nvidia-smi -i ${gpu_id} -lms=1 --query-gpu=timestamp,utilization.gpu,power.draw,memory.used,memory.total --format=csv,noheader,nounits >> logs/gpu_usage_node${SLURM_NODEID}_gpu${gpu_id}.log
+#       sleep 10
+#     done &
+#   done
+# }
